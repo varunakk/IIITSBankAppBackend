7cb185a4b64d1057a673c498cec06108d85eb925
varunakk <56592896+varunakk@users.noreply.github.com>
2022-04-27 02:44:18 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -101,7 +101,7 @@ const dbt=require("./routers/DBTrans")
 app.use("/dbt",dbt);
 
 
-app.listen(process.env.PORT||5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
 
     console.log("server started");
 });
